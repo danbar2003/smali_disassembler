9bf9943d4c500f32a6d531eaeb630a82176e60b1
loani <loaniforsure>
2024-09-22 10:06:40 +0300

    making test to work using env variables

--- tests/simple_parse.rs
+++ tests/simple_parse.rs
@@ -4,28 +4,27 @@ use zip::ZipArchive;
 
 #[test]
 fn test_wow() -> Result<(), Box<dyn std::error::Error>> {
-    let zip_file = File::open("tests/duolingo.apk")?;
-    let mut file_data = vec![];
-    let mut archive = ZipArchive::new(zip_file)?;
+    let apk_file_path = std::env::var("APK_FILE").unwrap();
+    let zip_file = File::open(apk_file_path)?;
 
+    let mut archive = ZipArchive::new(zip_file)?;
     let mut dex_file = archive.by_name("classes.dex")?;
+
+    let mut file_data = vec![];
     dex_file.read_to_end(&mut file_data)?;
     let dex_object = dex::DexReader::from_vec(file_data)?;
 
-    for c in dex_object.classes() {
-        let c = c.unwrap();
-        for method in c.methods() {
-            let class_name = dex_object.get_type(c.id())?.to_string();
+    for class_res in dex_object.classes() {
+        let class = class_res.unwrap();
+        for method in class.methods() {
             if let Some(code) = method.code() {
-                println!("{}->{}", class_name, method.name().to_string());
                 let code: Vec<u8> = code
                     .insns()
                     .iter()
                     .flat_map(|num| num.to_ne_bytes())
                     .collect();
                 let decoder = SmaliDecoder::new(&code);
-                let please = decoder.decode_all();
-                return Ok(());
+                let _ = decoder.decode_all();
             }
         }
     }
